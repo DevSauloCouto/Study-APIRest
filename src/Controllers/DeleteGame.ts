797c399d997de8d9a0e6d3416da51d24f252a480
DevSauloCouto <saulocouto1221@gmail.com>
2023-12-30 02:29:31 -0300

    modificado retorno do json, adicionado apenas o item que foi deletado

--- src/Controllers/DeleteGame.ts
+++ src/Controllers/DeleteGame.ts
@@ -9,9 +9,11 @@ export class DeleteGame {
 
         const game: GamesProps | number = json.games.findIndex((game: GamesProps) => game.id == id);
 
+        const gameDel: GamesProps = json.games[game];
+
         if(game != -1){
             json.games.splice(game, 1);
-            return res.status(200).json({message: "Game Delete", games: json.games});
+            return res.status(200).json({message: "Game Delete", game: gameDel});
         }
         return res.status(404).json({message: "Not Found Game"});
     }
